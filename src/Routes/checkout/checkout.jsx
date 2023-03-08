53d90c42ee77fe685192fa93c9986a1355b4480d
Bundit Hirunwongsawang <bundit.hir@gmail.com>
2023-03-09 04:53:38 +0700

    cursor on + -

--- src/Routes/checkout/checkout.jsx
+++ src/Routes/checkout/checkout.jsx
@@ -16,9 +16,9 @@ const Checkout = () =>{
                             <p className="line-clamp-1">{name}</p>
                             <p className="uppercase md:hidden">thb {price.toFixed(2)}</p>
                             <div className="flex items-center justify-center border text-center md:hidden w-fit">
-                                <p className="px-2 " onClick={()=> RemovefromCart(item)}>-</p>
+                                <p className="px-2 cursor-pointer" onClick={()=> RemovefromCart(item)}>-</p>
                                 <p className="w-[50px] text-center border-x px-2">{quantity}</p>
-                                <p className="px-2" onClick={()=> AddtoCart(item)}>+</p>
+                                <p className="px-2 cursor-pointer" onClick={()=> AddtoCart(item)}>+</p>
                             </div>
                         </div>
                         
@@ -26,9 +26,9 @@ const Checkout = () =>{
                     <div className="flex items-center gap-[3rem]">
                         <p className="uppercase hidden md:block">thb {price.toFixed(2)}</p>
                         <div className="md:flex items-center justify-center border text-center hidden ">
-                            <p className="px-2 " onClick={()=> RemovefromCart(item)}>-</p>
+                            <p className="px-2 cursor-pointer" onClick={()=> RemovefromCart(item)}>-</p>
                             <p className="w-[30px] md:w-[50px] text-center border-x px-2">{quantity}</p>
-                            <p className="px-2" onClick={()=> AddtoCart(item)}>+</p>
+                            <p className="px-2 cursor-pointer" onClick={()=> AddtoCart(item)}>+</p>
                         </div>
                         <button onClick={()=> DeleteItemfromCart(item)}><DeleteIcon/></button>
                     </div>
